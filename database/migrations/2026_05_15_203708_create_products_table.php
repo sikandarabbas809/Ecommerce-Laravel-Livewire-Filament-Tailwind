00dfbf3f44a409ba915022f8fa6340cabaaf16dc
Sikandar Abbas <sikandarabbas809@gmail.com>
2026-05-30 23:40:17 +0100

    prduct resource crud

--- database/migrations/2026_05_15_203708_create_products_table.php
+++ database/migrations/2026_05_15_203708_create_products_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->boolean('is_active')->default(true);
             $table->boolean('is_featured')->default(false);
             $table->boolean('is_stock')->default(true);
-            $table->boolean('on_Sale')->default(false);
+            $table->boolean('on_sale')->default(false);
             $table->timestamps();
         });
     }
