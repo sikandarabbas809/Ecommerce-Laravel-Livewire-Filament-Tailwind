67858028ab6a3dbfbb059c5115e7453f2752e125
Sikandar Abbas <sikandarabbas809@gmail.com>
2026-06-09 23:40:00 +0100

    order resource changes

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\OrderResource\Pages;
 use App\Filament\Resources\OrderResource\RelationManagers;
 use App\Models\Order;
+use App\Models\Product;
 use Filament\Forms;
 use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
@@ -15,10 +16,15 @@ use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Placeholder;
 use Filament\Forms\Components\Repeater;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\ToggleButtons;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Illuminate\Support\Number;
 
 use function Laravel\Prompts\select;
 
@@ -114,26 +120,52 @@ class OrderResource extends Resource
                             ->preload()
                             ->distinct()
                             ->disableOptionsWhenSelectedInSiblingRepeaterItems()
-                            ->columnSpan(4),
+                            ->columnSpan(4)
+                            ->reactive()
+                            ->afterStateUpdated(fn ($state, Set $set) => $set('unit_amount', 
+                            Product::find($state)?->price ?? 0))
+                            ->afterStateUpdated(fn ($state, Set $set) => $set('total_amount', 
+                            Product::find($state)?->price ?? 0)),
 
                             TextInput::make('quantity')
                             ->required()
                             ->numeric()
                             ->default(1)
                             ->minValue(1)
-                            ->columnSpan(2),
-
+                            ->columnSpan(2)
+                            ->reactive()
+                            ->afterStateUpdated(fn($state, Set $set, Get $get ) => $set('total_amount',
+                            $state * $get('unit_amount'))),
+                            
                             TextInput::make('unit_amount')
                             ->required()
                             ->numeric()
                             ->disabled()
+                            ->dehydrated()
                             ->columnSpan(3),
 
                             TextInput::make('total_amount')
                             ->required()
                             ->numeric()
+                            ->dehydrated()
                             ->columnSpan(3)
-                        ])->columns(12)
+                        ])->columns(12),
+
+                        Placeholder::make('grand_total_placeholder')
+                        ->label('Grand Total')
+                        ->content(function(Get $get, Set $set){
+                            $total = 0;
+                            if(!$repeaters = $get('items')){
+                                return $total;
+                            }
+                            foreach($repeaters as $key => $repeater){
+                                $total += $get("items.{$key}.total_amount");
+                            }
+                            $set('grand_total', $total);
+                            return Number::currency($total, 'GBP');
+                        }),
+                        Hidden::make('grand_total')
+                        ->default(0)
                     ])
                 ])->columnSpanFull()
             ]);
